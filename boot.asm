3e6d10a4badd8ef1a1c554620a6177010a772962
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-05-15 10:46:15 +0200

    Enter longmode

--- boot.asm
+++ boot.asm
@@ -3,6 +3,38 @@
   section .text
   bits 32
 start:
+  ;; Point the first entry of the level four page table to the first entry in
+  ;; the p3 table
+  mov eax, p3_table
+  or eax, 0b11
+  move dword [p4_table + 0], eax
+  ;; Point the first entry of the level three page table to the first entry in
+  ;; the p2 table
+  mov eax, p2_table
+  or eax, 0b11
+  move dword [p3_table + 0], eax
+  ;; Point each level two page table entry to a page
+  mov ecx, 0                    ; counter variable
+.map_p2_table:
+  uov eax 0x2000000             ; 2Mib
+  mul ecx
+  or eax, 0b10000011
+  mov [p2_table + ecx * 8], eax
+  inc ecx
+  cmp ecx, 512
+  jne .map_p2_table
+  mov eax, p4_table
+  mov cr3, eax
+  ;; enable Physical Address Extension (PAE)
+  mov eax, cr4
+  or eax, 1 << 5
+  mov cr4, eax
+  ;; set the long mode bit
+  mov eax, cr0
+  or eax, 1 << 31
+  or eax, 1 << 16
+  mov cr0, eax
+  ;; Greet
   mov word [0xb8000], 0x0248    ; H
   mov word [0xb8002], 0x0265    ; e
  	mov word [0xb8004], 0x026c    ; l
@@ -17,3 +49,13 @@ start:
  	mov word [0xb8016], 0x0264    ; d
  	mov word [0xb8018], 0x0221    ; !
   hlt
+
+  section .bss
+  align 4096
+
+p4_table:
+  resb 4096
+p3_table:
+  resb 4096
+p2_table:
+  resb 4096
